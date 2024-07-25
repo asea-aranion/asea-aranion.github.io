9d3753bb9dcf0dc31fe0244df26f13e1205535a1
asea-aranion <mochalatte@cox.net>
2024-07-25 10:58:54 -0700

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function appMouseOver(id) {
-    document.getElementById(id).style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0))"
+function appMouseOver(index) {
+    document.getElementsByClassName("app")[index].style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0))"
 }
