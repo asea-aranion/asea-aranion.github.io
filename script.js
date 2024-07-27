630f2c211230c65254dbfeb115fe64013b529ecc
asea-aranion <mochalatte@cox.net>
2024-07-27 14:13:04 -0700

    hover improvements

--- script.js
+++
@@ -1,5 +0,0 @@
-function appMouseOver(id) {
-    document.getElementById(id).style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))"
-}
-const clockApp = document.getElementById("underwaterclock")
-clockApp.onmouseover = appMouseOver("underwaterclock")
